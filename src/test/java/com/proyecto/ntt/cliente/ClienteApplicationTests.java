718f5a802b3c89a6202b9970ddc82903977fbcf2
Luz María <luzmar_80@hotmail.com>
2024-11-14 17:05:48 -0500

    cliente

---
+++ src/test/java/com/proyecto/ntt/cliente/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.ntt.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
